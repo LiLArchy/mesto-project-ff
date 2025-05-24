cd3de8e989b305d333d1849bd1533297d1869a79
LiLArchy <isysartur@yandex.ru>
2025-05-24 17:30:22 +0300

    ready to push

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+
+function cardAdd (cardItems, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item ').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = cardItems.link;
+    cardElement.querySelector('.card__image').alt = cardItems.name;
+    cardElement.querySelector('.card__title').src = cardItems.name;
+
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+
+}
 
 // @todo: Функция удаления карточки
 
+const deleteCard = (evt) => {
+    evt.target.closest('.card').remove();
+};
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((items => {
+    const card = cardAdd(items, deleteCard);
+    cardList.append(card);
+}));
